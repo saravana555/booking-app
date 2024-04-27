919388478a59397d40d312bf0c0cbba9913f9cd7
saravana555 <saravanankrishnakumar05@gmail.com>
2024-04-27 13:25:45 +0530

    added search feature and e2e tests

--- backend/src/shared/types.ts
+++ backend/src/shared/types.ts
@@ -14,4 +14,13 @@ export type HotelType = {
     imageUrls: string[];
     lastUpdated: Date;
 
-}
+};
+
+export type HotelSearchResponse = {
+    data: HotelType[];
+    pagination: {
+        total: number;
+        page: number;
+        pages: number;
+    };
+};
